8d179da6fd39cd436b0a397638e4fa15fe1eb0b9
HanzzSama <haidarandrianno098@gmail.com>
2026-04-13 11:28:40 +0700

    Migration CSS(1)

--- resources/views/auth/index.blade.php
+++ resources/views/auth/index.blade.php
@@ -1,15 +1,21 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    @vite(['resources/css/global.css', 'resources/css/welcome.css'])
+    <!-- @vite(['resources/css/global.css', 'resources/css/welcome.css']) -->
+    <link rel="stylesheet" href="{{ asset('css/global.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/welcome.css') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
     <title>{{ config('app.name') }}</title>
 </head>
+
 <body>
     <div class="back-cover">
         <h1>Turning Code</h1>
@@ -44,4 +50,5 @@
         </main>
     </div>
 </body>
+
 </html>
